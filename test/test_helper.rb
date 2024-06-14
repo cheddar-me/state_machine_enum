95eac8090f74529f787c1217e977039546be9440
Stanislav Katkov <skatkov@cheddar.me>
2024-06-14 18:15:35 +0200

    Adding basic tests for a gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,22 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "state_machine_enum"
 
 require "minitest/autorun"
+require "active_support"
+require "active_support/test_case"
+require "active_record"
+
+class ActiveSupport::TestCase
+  def setup
+    ActiveRecord::Migration.verbose = false
+    ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+    ActiveRecord::Schema.define do
+      create_table :users, force: true do |t|
+        t.string :state, default: 'registered'
+      end
+    end
+  end
+
+  def teardown
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
