a75732df9d51e9afcdd13a9c8db97d2b590c6483
Stanislav (Stas) Katkov <skatkov@cheddar.me>
2024-06-18 12:23:42 +0200

    Adding basic tests (#3)
    - Adding basic tests.
    - #ensure_state_may_transition_to! method now actually verifies that an attribute can transition to a new state.
    - Adding CHANGELOG.md
    
    ---------
    
    Co-authored-by: Stanislav (Stas) Katkov <github@skatkov.com>

--- state_machine_enum.gemspec
+++ state_machine_enum.gemspec
@@ -14,11 +14,9 @@ Gem::Specification.new do |spec|
   spec.license = "MIT"
   spec.required_ruby_version = ">= 3.0.0"
 
-  # spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
-
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = "https://github.com/cheddar-me/state_machine_enum"
-  #spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["changelog_uri"] = "https://github.com/cheddar-me/state_machine_enum/blob/main/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -29,9 +27,9 @@ Gem::Specification.new do |spec|
         f.start_with?(*%w[bin/ test/ spec/ features/ .git .github appveyor Gemfile])
     end
   end
-  spec.bindir = "exe"
-  spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "activesupport", ">= 6.0"
+  spec.add_dependency "activesupport", "~> 7"
+  spec.add_development_dependency "sqlite3", "~> 1.4"
+  spec.add_development_dependency "activerecord", "~> 7"
 end
--- test/test_state_machine_enum.rb
+++ test/test_state_machine_enum.rb
@@ -2,12 +2,72 @@
 
 require "test_helper"
 
-class TestStateMachineEnum < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::StateMachineEnum::VERSION
+class TestStateMachineEnum < ActiveSupport::TestCase
+  class User < ActiveRecord::Base
+    include StateMachineEnum
+
+    state_machine_enum :state do |s|
+      s.permit_transition(:registered, :active)
+      s.permit_transition(:active, :banned)
+      s.permit_transition(:banned, :active)
+      s.permit_transition(:active, :deleted)
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_state_transitions
+    u = User.create!
+
+    assert u.active!
+    assert u.banned!
+    assert u.active!
+    assert u.deleted!
+  end
+
+  def test_callbacks
+    u = User.create!
+
+    assert u.active!
+    error = assert_raises(ActiveRecord::RecordInvalid) { u.registered! }
+    assert_equal("Validation failed: State Invalid transition from active to registered", error.message)
+
+    error = assert_raises(ArgumentError) { u.update!(state: "missing") }
+    assert_equal("'missing' is not a valid state", error.message)
+  end
+
+  def test_ensure_state_may_transition_to!
+    u = User.create!(state: "active")
+
+    assert_nil u.ensure_state_may_transition_to!(:banned)
+
+    assert_raises(StateMachineEnum::InvalidState) { u.ensure_state_may_transition_to!("active") }
+    error = assert_raises(StateMachineEnum::InvalidState) { u.ensure_state_may_transition_to!(:active) }
+    assert_equal("state already is \"active\"", error.message)
+
+    error = assert_raises(StateMachineEnum::InvalidState) { u.ensure_state_may_transition_to!(:registered) }
+    assert_equal("state may not transition from \"active\" to :registered", error.message)
+  end
+
+  def test_ensure_state_one_of!
+    u = User.create!(state: "active")
+
+    assert_nil u.ensure_state_one_of!(:active, :banned)
+    assert_nil u.ensure_state_one_of!("active", "banned")
+
+    error = assert_raises(StateMachineEnum::InvalidState) { u.ensure_state_one_of!(:banned, :deleted) }
+    assert_equal("state must be one of [:banned, :deleted] but was \"active\"", error.message)
+
+    error = assert_raises(StateMachineEnum::InvalidState) { u.ensure_state_one_of!("banned", "deleted") }
+    assert_equal("state must be one of [\"banned\", \"deleted\"] but was \"active\"", error.message)
+  end
+
+  def test_may_transition_to?
+    u = User.create!
+
+    assert u.active!
+
+    assert u.state_may_transition_to?(:banned)
+    assert u.state_may_transition_to?("banned")
+    refute u.state_may_transition_to?(:registered)
+    refute u.state_may_transition_to?(:missing)
   end
 end
